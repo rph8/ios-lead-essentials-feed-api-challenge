80aa7ae069bf5aff1b5b03922086fe63de643f62
Raphael Haase <raphael@sylvilagus.com>
2021-07-06 19:42:30 +0200

    Simplify code for passing test_loadTwice_requestsDataFromURLTwice

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -19,22 +19,11 @@ public final class RemoteFeedLoader: FeedLoader {
 	}
 
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
-		client.get(from: url) { [unowned self] result in
-			switch result {
-			case .failure(_):
-				// Do nothing
-				break
-			case .success(_):
-				self.client.get(from: self.url) { result in
-					switch result {
-					case .failure(_):
-						// Do nothing
-						break
-					case .success(_):
-						completion(.success([FeedImage(id: UUID(), description: nil, location: nil, url: URL(string: "https://a-given-url.com")!)]))
-					}
-				}
+		client.get(from: self.url) { result in
+			guard case .success = result else {
+				return
 			}
+			completion(.success([FeedImage(id: UUID(), description: nil, location: nil, url: URL(string: "https://a-given-url.com")!)]))
 		}
 	}
 }
