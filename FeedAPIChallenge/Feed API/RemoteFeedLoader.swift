ab1c385782a6481cda3f81d68ce1f7d652a4de24
Raphael Haase <raphael@sylvilagus.com>
2021-07-21 11:59:26 +0200

    Make test_load_deliversInvalidDataErrorOnNon200HTTPResponse work

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -24,6 +24,10 @@ public final class RemoteFeedLoader: FeedLoader {
 				completion(.failure(Error.connectivity))
 				return
 			}
+			guard let (_, httpResponse) = try? result.get(), httpResponse.statusCode == 200 else {
+				completion(.failure(Error.invalidData))
+				return
+			}
 			completion(.success([FeedImage(id: UUID(), description: nil, location: nil, url: URL(string: "https://a-given-url.com")!)]))
 		}
 	}
